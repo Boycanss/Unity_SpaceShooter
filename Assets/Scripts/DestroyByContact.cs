61b330ff0c166f6683eb2ac5a42e1f3737629f74
boycanss <boy.simanjuntak49@gmail.com>
2022-11-11 18:08:52 +0700

    - Added explosion effect to asteroid and player on collide

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Boundary"){
             return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if(other.tag == "Player"){
+            Instantiate(playerExplosion, transform.position, transform.rotation);
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
